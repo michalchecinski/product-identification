05f62907b0ef60f913102234c19131432bd4c3b1
Michał Chęciński <michal.checinski@outlook.com>
2019-06-15 21:30:50 +0200

    Add data and core projects for entites and db

---
+++ src/ProductIdentification.Core/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductIdentification.Core.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal NetPrice { get; set; }
+        public decimal GrossPrice { get; set; }
+        public int SubCategoryId { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public int CategoryId { get; set; }
+        public SubCategory Category { get; set; }
+    }
+}
---
+++ src/ProductIdentification.Data/ProductIdentificationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProductIdentification.Core.Models;
+
+namespace ProductIdentification.Data
+{
+    public class ProductIdentificationContext : IdentityDbContext
+    {
+        public ProductIdentificationContext(DbContextOptions<ProductIdentificationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+    }
+}
