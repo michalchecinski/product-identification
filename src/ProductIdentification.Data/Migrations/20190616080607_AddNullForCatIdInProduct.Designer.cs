427ca37772eec6c67caae6566850c58b48bfa81a
Michał Chęciński <michal.checinski@outlook.com>
2019-06-16 10:25:02 +0200

    Add working migrations

--- src/ProductIdentification.Data/Migrations/20190616080607_AddNullForCatIdInProduct.Designer.cs
+++ src/ProductIdentification.Data/Migrations/20190616080607_AddNullForCatIdInProduct.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductIdentification.Data;
 
 namespace ProductIdentification.Data.Migrations
 {
     [DbContext(typeof(ProductIdentificationContext))]
-    partial class ProductIdentificationContextModelSnapshot : ModelSnapshot
+    [Migration("20190616080607_AddNullForCatIdInProduct")]
+    partial class AddNullForCatIdInProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/ProductIdentification.Data/ProductIdentificationContext.cs
+++ src/ProductIdentification.Data/ProductIdentificationContext.cs
@@ -14,5 +14,36 @@ namespace ProductIdentification.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<SubCategory> SubCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Product>()
+            //            .HasOne(p => p.Category)
+            //            .WithMany(c => c.Products)
+            //            .HasForeignKey(x => x.CategoryId)
+            //            .OnDelete(DeleteBehavior.Restrict);
+
+            //modelBuilder.Entity<Product>()
+            //            .HasIndex(p => p.CategoryId)
+            //            .IsUnique(false);
+
+            modelBuilder.Entity<Product>()
+                        .HasOne(p => p.SubCategory)
+                        .WithMany(s => s.Products)
+                        .HasForeignKey(x => x.SubCategoryId)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Product>()
+                        .HasIndex(p => p.SubCategoryId)
+                        .IsUnique(false);
+
+            modelBuilder.Entity<SubCategory>()
+                        .HasOne(s => s.Category)
+                        .WithMany(c => c.SubCategories)
+                        .HasForeignKey(s => s.CategoryId)
+                        .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
