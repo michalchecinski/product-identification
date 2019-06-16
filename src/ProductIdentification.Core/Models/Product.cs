0aebddd65af112b1fe5759e4e485a9aad397943b
Michał Chęciński <michal.checinski@outlook.com>
2019-06-16 10:45:12 +0200

    Sorted usings

--- src/ProductIdentification.Core/Models/Product.cs
+++ src/ProductIdentification.Core/Models/Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProductIdentification.Core.Models
+﻿namespace ProductIdentification.Core.Models
 {
     public class Product : BaseEntity
     {
