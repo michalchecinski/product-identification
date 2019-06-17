f0a6fff6dcaa000eee7eb8b647d16ee279ba1613
Michał Chęciński <michal.checinski@outlook.com>
2019-06-17 23:39:56 +0200

    Add script to migrate migrations

---
+++ src/ProductIdentification.AzureRG/Scripts/migration.ps1
@@ -0,0 +1,28 @@
+﻿param([String]$file="DataContext.sql")
+
+$regexA = '(CREATE PROCEDURE(.*?\s?)(?=END;))'
+$regexB = '(ALTER PROCEDURE(.*?\s?)(?=END;))'
+$encoding = New-Object System.Text.UTF8Encoding
+
+
+Get-ChildItem  $file | % {
+  $c = (Get-Content $_.FullName) -replace "`n"," " -replace "`r"," "
+  [IO.File]::WriteAllText("$((Get-Item -Path ".\").FullName)\\"+$file, $c, $encoding)
+}
+Get-ChildItem $file | % {
+  $c = (Get-Content $_.FullName) -replace $regexA,'EXEC(''$0'')'
+  [IO.File]::WriteAllText("$((Get-Item -Path ".\").FullName)\\"+$file, $c, $encoding)
+}
+Get-ChildItem $file | % {
+    $c = (Get-Content $_.FullName) -replace $regexB,'EXEC(''$0'')'
+    [IO.File]::WriteAllText("$((Get-Item -Path ".\").FullName)\\"+$file, $c, $encoding)
+  }
+  Get-ChildItem $file | % {
+    $c = (Get-Content $_.FullName) -replace '\s\s\s\s',"`r`n"
+    [IO.File]::WriteAllText("$((Get-Item -Path ".\").FullName)\\"+$file, $c, $encoding)
+  }
+  Get-ChildItem $file | % {
+    $c = (Get-Content $_.FullName) -replace 'GO',"`r`nGO`r`n"
+    [IO.File]::WriteAllText("$((Get-Item -Path ".\").FullName)\\"+$file, $c, $encoding)
+  }
+Write-Host ($file+" has been fixed")
