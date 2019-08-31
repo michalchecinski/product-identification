f958534414fab2963b6a2e2376a503fd0b711a92
Michał Chęciński <michal.checinski@outlook.com>
2019-08-31 12:47:03 +0200

    Add queue service

---
+++ src/ProductIdentification.Infrastructure/IQueueService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ProductIdentification.Core.Models.Messages;
+
+namespace ProductIdentification.Infrastructure
+{
+    public interface IQueueService
+    {
+        Task SendMessageAsync(string queueName, IQueueMessage message);
+    }
+}
---
+++ src/ProductIdentification.Infrastructure/QueueService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ProductIdentification.Core.Models.Messages;
+
+namespace ProductIdentification.Infrastructure
+{
+    public class QueueService : IQueueService
+    {
+        public Task SendMessageAsync(string queueName, IQueueMessage message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
