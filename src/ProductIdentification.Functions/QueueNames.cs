5464d7d695eed967e72f9e11c83a7921457f7627
Michał Chęciński <michal.checinski@outlook.com>
2019-08-31 12:47:16 +0200

    Add queue names class

---
+++ src/ProductIdentification.Functions/QueueNames.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductIdentification.Functions
+{
+    public static class QueueNames
+    {
+        public const string PublishModel = "publish-model";
+        public const string TrainModel = "train-model";
+    }
+}
