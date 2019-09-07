89c79a31d0630391e283f5b42d8687acbcd2feef
Michał Chęciński <michal.checinski@outlook.com>
2019-09-07 21:56:25 +0200

    Add common proj with extension methods

---
+++ src/ProductIdentification.Common/ProductExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ProductIdentification.Core.DomainModels;
+
+namespace ProductIdentification.Common
+{
+    public static class ProductExtensions
+    {
+        public static string StoragePath(this Product product)
+        {
+            return $"{product.Category.Name}/{product.SubCategory.Name}/{product.Name}".Replace(" ", "-");
+        }
+        
+        public static string StoragePathOriginal(this Product product)
+        {
+            return $"{product.StoragePath()}/original";
+        }
+    }
+}
