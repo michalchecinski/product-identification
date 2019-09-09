4fd639e2a6a68a1fd7fb834d3b05239aac7f2da0
Michał Chęciński <michal.checinski@outlook.com>
2019-09-09 22:09:35 +0200

    Add review product Dto

---
+++ src/ProductIdentification.Core/Models/ReviewProductPhotosDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ProductIdentification.Core.DomainModels;
+
+namespace ProductIdentification.Core.Models
+{
+    public class ReviewProductPhotosDto
+    {
+        public Product Product { get; set; }
+        public IEnumerable<PhotoFile> Photos { get; set; }
+    }
+}
