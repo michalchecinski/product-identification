777d934fa3870b15af749209c910108d7971bbdc
Michał Chęciński <michal.checinski@outlook.com>
2019-09-09 17:50:18 +0200

    Add task extension method

---
+++ src/ProductIdentification.Common/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProductIdentification.Common
+{
+    public static class TaskExtensions
+    {
+        public static async void RunAndForget(
+            this Task task, Action<Exception> onException = null)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                onException?.Invoke(ex);
+            }
+        }
+    }
+}
