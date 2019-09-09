fbe8b85c04f36cc4964db6676d0315d29d5d08db
Michał Chęciński <michal.checinski@outlook.com>
2019-09-09 22:08:45 +0200

    Add update product message

---
+++ src/ProductIdentification.Core/Models/Messages/UpdateProductMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductIdentification.Core.Models.Messages
+{
+    public class UpdateProductMessage : IQueueMessage
+    {
+        public UpdateProductMessage(int productId)
+        {
+            ProductId = productId;
+        }
+        
+        public int ProductId { get; }
+    }
+}
