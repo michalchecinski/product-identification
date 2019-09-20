a57115b0ec96d89b480b56c1560e24ad8fae84f1
Michał Chęciński <michal.checinski@outlook.com>
2019-09-20 20:27:06 +0200

    Create Role names dictrionary

---
+++ src/ProductIdentification.Core/Models/Roles/RoleNameDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ProductIdentification.Core.Models.Roles
+{
+    public static class RoleNameDictionary
+    {
+        public static Dictionary<string, string> All => new Dictionary<string, string>()
+        {
+            {Role.Admin, nameof(Role.Admin)},
+            {Role.Manager, nameof(Role.Manager)},
+            {Role.DataManager, nameof(Role.DataManager)},
+            {Role.WarehouseMan, nameof(Role.WarehouseMan)}
+        };
+
+        public static Dictionary<string, string> BelowManager => new Dictionary<string, string>()
+        {
+            {Role.DataManager, nameof(Role.DataManager)},
+            {Role.WarehouseMan, nameof(Role.WarehouseMan)}
+        };
+    }
+}
