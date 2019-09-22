f11efcc396ce9c1fc7f4de24eadb1731d96ea041
Michał Chęciński <michal.checinski@outlook.com>
2019-09-22 18:06:13 +0200

    Add email message model

---
+++ src/ProductIdentification.Core/Models/Messages/SendEmailMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductIdentification.Core.Models.Messages
+{
+    public class SendEmailMessage : IQueueMessage
+    {
+        public SendEmailMessage(string email, string title, string htmlMessage)
+        {
+            Email = email;
+            Title = title;
+            HtmlMessage = htmlMessage;
+        }
+        
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string HtmlMessage { get; set; }
+    }
+}
