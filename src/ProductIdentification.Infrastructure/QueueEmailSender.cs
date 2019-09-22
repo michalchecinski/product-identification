73433beaf2941a76c55a19fc4c44476ca250de97
Michał Chęciński <michal.checinski@outlook.com>
2019-09-22 18:06:29 +0200

    Implement queue email sender

---
+++ src/ProductIdentification.Infrastructure/QueueEmailSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ProductIdentification.Core.Models.Messages;
+
+namespace ProductIdentification.Infrastructure
+{
+    public class QueueEmailSender : IEmailSender
+    {
+        private readonly IQueueService _queueService;
+
+        public QueueEmailSender(IQueueService queueService)
+        {
+            _queueService = queueService;
+        }
+        
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailMessage = new SendEmailMessage(email, subject, htmlMessage);
+            await _queueService.SendMessageAsync(QueueNames.SendEmail, emailMessage);
+        }
+    }
+}
