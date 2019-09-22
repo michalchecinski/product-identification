4fb5ef86f43fec11ab8a5fa7c1f28d7279eedbda
Michał Chęciński <michal.checinski@outlook.com>
2019-09-22 18:08:54 +0200

    Add email sender functions

---
+++ src/ProductIdentification.Functions/EmailSenderFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using ProductIdentification.Core.Models.Messages;
+using ProductIdentification.Infrastructure;
+
+namespace ProductIdentification.Functions
+{
+    public class EmailSenderFunction
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailSenderFunction(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+        
+        [FunctionName(nameof(EmailSender))]
+        public void EmailSender([QueueTrigger(QueueNames.SendEmail, Connection = "Storage")]
+                               SendEmailMessage emailMessage, ILogger log)
+        {
+            log.LogInformation($"Sending email to: {emailMessage.Email} {emailMessage.Title}");
+
+            _emailService.SendEmail(emailMessage.Email, emailMessage.Title, emailMessage.HtmlMessage);
+            
+            log.LogInformation($"Email sent to: {emailMessage.Email} {emailMessage.Title}");
+        }
+    }
+}
