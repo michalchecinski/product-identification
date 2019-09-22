3dbf62ca4dccc0bc31db03514f7e5d0a893ce34e
Michał Chęciński <michal.checinski@outlook.com>
2019-09-22 18:06:49 +0200

    Add email service...
    for sending email

---
+++ src/ProductIdentification.Infrastructure/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ProductIdentification.Infrastructure
+{
+    public class EmailService : IEmailService
+    {
+        private readonly string _fromEmail;
+        private readonly string _emailPassword;
+        
+        public EmailService(AppSettings settings)
+        {
+            _fromEmail = settings.EmailFrom;
+            _emailPassword = settings.EmailPassword;
+        }
+        
+        public async Task SendEmail(string email, string title, string htmlMessage)
+        {
+            MailMessage message = new MailMessage(); 
+            message.From = new MailAddress("FromMailAddress");  
+            message.To.Add(new MailAddress(email));  
+            message.Subject = title;  
+            message.IsBodyHtml = true;
+            message.Body = htmlMessage;
+            
+            var smtp = GetSmtpClient();
+            smtp.Send(message); 
+        }
+
+        private static SmtpClient GetSmtpClient()
+        {
+            SmtpClient smtp = new SmtpClient();
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("FromMailAddress", "password");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            return smtp;
+        }
+    }
+}
---
+++ src/ProductIdentification.Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ProductIdentification.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string email, string title, string htmlMessage);
+    }
+}
